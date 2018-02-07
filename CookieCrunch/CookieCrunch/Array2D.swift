fc0af70142bea27c48d44b00a61ae04f4e898316
Nicola Solazzo <niksolaz@iMac-di-Nicola.local>
2018-02-07 18:42:31 +0100

    Add array two dimensional

---
+++ CookieCrunch/CookieCrunch/Array2D.swift
@@ -0,0 +1,28 @@
+//
+//  Array2D.swift
+//  CookieCrunch
+//
+//  Created by Nicola Solazzo on 07/02/18.
+//  Copyright © 2018 Nicola Solazzo. All rights reserved.
+//
+
+struct Array2D<T> {
+    let columns: Int
+    let rows: Int
+    fileprivate var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(repeating: nil, count: rows*columns)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
