8aea1904990adb0ce1dcfef45c98fb86ab6e368e
Nicola Solazzo <niksolaz@iMac-di-Nicola.local>
2018-02-07 17:47:36 +0100

    Modified Base Scene

--- CookieCrunch/CookieCrunch/GameViewController.swift
+++ CookieCrunch/CookieCrunch/GameViewController.swift
@@ -8,48 +8,34 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+    var scene: GameScene!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return [.portrait, .portraitUpsideDown]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure the view.
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene.
+        skView.presentScene(scene)
     }
 }
